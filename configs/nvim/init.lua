9eb335f1ae3423e9c166f6563620a6ac320c6601
Anshuman Rai <anshumancos3@gmail.com>
2026-08-04 22:00:18 +0530

    chore: auto-sync mintbtw 2026-08-04 22:00

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -1,191 +1,155 @@
--- Set leader key to Space (must be set before lazy.nvim)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- ==========================================================================
+-- NEOVIM MINIMAL CONFIGURATION
+-- Focused on Python, Lua, and Bash
+-- ==========================================================================
 
--- Bootstrap lazy.nvim if not installed
+-- 1. CORE OPTIONS
+vim.g.mapleader = " "              -- Set space as the leader key
+vim.opt.number = true              -- Show line numbers
+vim.opt.relativenumber = true      -- Use relative line numbers for easy jumping
+vim.opt.expandtab = true           -- Convert tabs to spaces (PEP 8 standard for Python)
+vim.opt.shiftwidth = 4             -- 4 spaces per indentation
+vim.opt.tabstop = 4                -- 4 spaces per tab
+vim.opt.wrap = false               -- Disable line wrapping
+vim.opt.ignorecase = true          -- Ignore case when searching
+vim.opt.smartcase = true           -- Match case if search contains a capital letter
+vim.opt.termguicolors = true       -- Enable true color support
+vim.opt.clipboard = "unnamedplus"  -- Sync with Linux system clipboard
+vim.opt.undofile = true            -- Maintain undo history between sessions
+vim.opt.splitbelow = true          -- Horizontal splits open below
+vim.opt.splitright = true          -- Vertical splits open to the right
+
+-- 2. BASIC KEYMAPS
+local keymap = vim.keymap.set
+keymap("n", "<leader>w", "<cmd>w<CR>", { desc = "Save File" })
+keymap("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit Neovim" })
+keymap("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear Search Highlights" })
+
+-- Better window navigation (Ctrl + hjkl)
+keymap("n", "<C-h>", "<C-w>h", { desc = "Move to left split" })
+keymap("n", "<C-j>", "<C-w>j", { desc = "Move to lower split" })
+keymap("n", "<C-k>", "<C-w>k", { desc = "Move to upper split" })
+keymap("n", "<C-l>", "<C-w>l", { desc = "Move to right split" })
+
+-- 3. BOOTSTRAP PLUGIN MANAGER (lazy.nvim)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Plugin setup
+-- 4. PLUGINS
 require("lazy").setup({
--- 1. Theme: Catppuccin
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    lazy = false, -- ADDED: This strictly forces it to load before anything else
-    config = function()
-      require("catppuccin").setup({
-        flavour = "mocha",
-        transparent_background = false,
-        integrations = {
-          treesitter = true,
-          native_lsp = { enabled = true },
-          telescope = true,
-          nvimtree = true,
-        },
-      })
-      vim.cmd.colorscheme("catppuccin")
-    end,
-  },
-
-  -- 2. Status line
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup({
-        options = {
-          theme = "auto", -- CHANGED: "auto" immediately inherits the active colorscheme
-          section_separators = { left = '', right = '' },
-          component_separators = { left = '', right = '' },
-        },
-      })
-    end,
-  },
-
-    -- 3. Fuzzy finder (Telescope)
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    keys = {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
-      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Find Buffers" },
-    },
-  },
-
-  -- 4. Treesitter for syntax highlighting
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "sql", "c", "cpp", "lua", "vim", "markdown" },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-
-  -- 5. File explorer
-  {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    keys = {
-      { "<leader>e", "<cmd>NvimTreeToggle<CR>", desc = "Toggle Explorer" },
-    },
-    config = function()
-      require("nvim-tree").setup({
-        view = { width = 30, side = "left" },
-        renderer = { group_empty = true },
-      })
-    end,
-  },
-
-  -- 6. Commenting
-  {
-    "numToStr/Comment.nvim",
-    config = function()
-      require("Comment").setup()
-    end,
-  },
-
--- 7. Codeium (Free AI Autocomplete)
-  {
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-    config = function()
-      -- Disable default tab if it conflicts, but usually it works out of the box
-      vim.g.codeium_disable_bindings = 1
-      
-      -- Set your own keybindings
-      -- Press Tab to accept the suggestion
-      vim.keymap.set('i', '<Tab>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
-      -- Press Alt+] to cycle to the next suggestion
-      vim.keymap.set('i', '<M-]>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
-      -- Press Alt+[ to cycle to the previous suggestion
-      vim.keymap.set('i', '<M-[>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
-      -- Press Ctrl+x to dismiss a suggestion you don't want
-      vim.keymap.set('i', '<C-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
-    end
-  },
-
-  -- 8. LeetCode plugin
-  {
-    "kawre/leetcode.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      lang = "python3",
-      storage = {
-        home = vim.fn.expand("~/everything/learning/job_prep/dsa/leetcode_sols"),
-      },
-    },
-  },
-
--- 9. Native LSP Config (Neovim 0.11+ standard)
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      -- Start the LSP servers using Neovim's native engine
-      -- (Still requires pyright and clangd installed on your OS)
-      vim.lsp.enable("pyright")
-      vim.lsp.enable("clangd")
-      
-      -- Attach keymaps only when an LSP connects to a buffer
-      vim.api.nvim_create_autocmd('LspAttach', {
-        callback = function(args)
-          local opts = { buffer = args.buf }
-          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    -- Clean UI Theme
+    {
+        "navarasu/onedark.nvim",
+        config = function()
+            vim.cmd.colorscheme("onedark")
         end,
-      })
-    end,
-  },
+    },
 
-  -- NEW: 10. Indent guides (Crucial for Python)
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    config = function()
-      require("ibl").setup()
-    end,
-  },
+    -- Syntax Highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "python", "lua", "bash", "markdown" },
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+    },
+
+    -- Fuzzy Finder for Files and Text
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        keys = {
+            { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find Files" },
+            { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live Grep (Search Text)" },
+        },
+    },
+
+    -- IntelliSense (LSP and Autocompletion)
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "williamboman/mason.nvim",           -- Installs language servers seamlessly
+            "williamboman/mason-lspconfig.nvim", -- Bridges mason with lspconfig
+            "hrsh7th/nvim-cmp",                  -- Autocompletion engine
+            "hrsh7th/cmp-nvim-lsp",              -- LSP source for cmp
+            "L3MON4D3/LuaSnip",                  -- Snippet engine
+        },
+        config = function()
+            -- 4a. Setup Mason to install language servers
+            require("mason").setup()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright", "lua_ls", "bashls", "ruff" },
+            })
+
+            -- 4b. Setup Keybindings for LSP features
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(event)
+                    local opts = { buffer = event.buf }
+                    vim.keymap.set("n", "gd", vim.lsp.buf.definition, vim.tbl_extend("force", opts, { desc = "Go to Definition" }))
+                    vim.keymap.set("n", "K", vim.lsp.buf.hover, vim.tbl_extend("force", opts, { desc = "Hover Documentation" }))
+                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, vim.tbl_extend("force", opts, { desc = "Rename Variable" }))
+                    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, vim.tbl_extend("force", opts, { desc = "Code Action" }))
+                end,
+            })
+
+            -- 4c. Setup the Language Servers
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            lspconfig.pyright.setup({ capabilities = capabilities })
+            lspconfig.ruff.setup({ capabilities = capabilities })
+            lspconfig.bashls.setup({ capabilities = capabilities })
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+                settings = {
+                    Lua = {
+                        diagnostics = { globals = { "vim" } },
+                    },
+                },
+            })
+
+            -- 4d. Setup Autocompletion UI and behavior
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Enter confirms selection
+                    ["<Tab>"] = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                }),
+            })
+        end,
+    },
 })
 
--- General Neovim options
-local opt = vim.opt
-
--- Line numbers
-opt.number = true
-opt.relativenumber = true
-
--- Indentation & Tabs
-opt.tabstop = 4       -- Updated for standard Python/C++ spacing
-opt.shiftwidth = 4    -- Updated for standard Python/C++ spacing
-opt.expandtab = true
-opt.smartindent = true
-
--- UI & Aesthetics
-opt.termguicolors = true
-opt.signcolumn = "yes" -- Prevents text shifting when errors appear
-opt.cursorline = true  -- Highlights the current line
-opt.scrolloff = 8      -- Keeps 8 lines visible above/below cursor when scrolling
-
--- Search & System
-opt.mouse = "a"
-opt.clipboard = "unnamedplus"
-opt.ignorecase = true  -- Case-insensitive search
-opt.smartcase = true   -- ...unless you type a capital letter
-opt.updatetime = 250   -- Faster completion and responsiveness
+-- 5. AUTO-COMMANDS
+-- Automatically format Python files on save using the active LSP (Ruff)
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.py",
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
