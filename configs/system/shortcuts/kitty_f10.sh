2ec4cd6665829771affd64c4681eb0e6e1532e9d
Anshuman Rai <anshumancos3@gmail.com>
2026-08-15 23:00:15 +0530

    chore: auto-sync mintbtw 2026-08-15 23:00

--- configs/system/shortcuts/kitty_f10.sh
+++ configs/system/shortcuts/kitty_f10.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+# 1. Identify the current workspace
 CURRENT_WS=$(wmctrl -d | awk '$2 == "*" {print $1}')
 
+# 2. Find the Kitty window ID strictly on the current workspace
 WIN=$(wmctrl -lx | \
       awk -v ws="$CURRENT_WS" '$2 == ws && $3 == "kitty.kitty" {print $1; exit}')
 
+# 3. Minimize all current windows to reveal the desktop wallpaper
+wmctrl -k on
+
+# 4. Give Cinnamon's window manager a tiny fraction of a second to process the animation
+sleep 0.15
+
+# 5. Bring Kitty back up
 if [ -n "$WIN" ]; then
+    # If it was already open (and just got minimized), this un-minimizes and focuses it
     wmctrl -ia "$WIN"
 else
+    # If it wasn't open, launch a fresh instance
     kitty &
 fi
